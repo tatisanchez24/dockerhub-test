b02f1b25b1de47053acf30e7f6d6b086d877e831
Tatiana Sanchez <tatianasanchez@seti.com.co>
2025-07-04 10:45:23 -0500

    🐞 fix(docker image): fix docker image
    docker image generation

--- generate_dockers.sh
+++ generate_dockers.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-REPO="tatianasanchez426/test1"
+mkdir -p build_context
 
 for file in docs/*.yaml; do
   name=$(basename "$file" .yaml)
-  cp Dockerfile.template Dockerfile
-  sed -i "s|__YAML_FILE__|$file|" Dockerfile
-  docker build -t "$REPO:$name" .
-  docker push "$REPO:$name"
+  cp Dockerfile.template build_context/Dockerfile
+  cp "$file" build_context/documentation.yaml
+
+  readme_path="docs_readme/${name}.md"
+  if [ -f "$readme_path" ]; then
+    cp "$readme_path" build_context/README.md
+  else
+    echo "# Documentación no disponible" > build_context/README.md
+  fi
+
+  docker build -t "tatianasanchez426/test1:$name" build_context
+  docker push "tatianasanchez426/test1:$name"
+  rm -rf build_context/*
 done
 
-rm Dockerfile
+rmdir build_context
